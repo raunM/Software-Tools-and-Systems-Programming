caf4415ee10dcfbd449ba7525ece2c86ad88b393
raunM <raun.mathur@gmail.com>
2016-02-27 04:28:15 -0500

    Added files via upload

--- Unix Shell/execute.c
+++ Unix Shell/execute.c
@@ -32,11 +32,6 @@ int execute_command(Command *cmd)
 	}
 
 	if (cmd->pipe_to == NULL) {
-		/* TODO: Implement executing a simple command without pipes.
-		*
-		* Use `fork` to run the function `run_program` in a child process, and
-		* wait for the child to terminate.
-		*/
 		pid_t child = fork();
 		//check for failure
 		if (child < 0) {
@@ -85,17 +80,6 @@ static int execute_builtin_cd(char *argv[])
 	char *arg = argv[1];
 	int is_absolute = (arg && arg[0] == '/');
 
-	/*
-		* TODO: Implement this function using `chdir(2)` and `getcwd(3)`.
-		*
-		* If the argument is absolute, you can pass it directly to chdir().
-		*
-		* If it is relative, you will first have to get the current working
-		* directory, append the relative path to it and chdir() to the resulting
-		* directory.
-		*
-		* This function should return whatever value chdir() returns.
-																*/
 	if(arg){
 		if(is_absolute){
 			return chdir(argv[1]);
@@ -142,25 +126,7 @@ static int execute_builtin_exit(char *argv[])
 */
 static int run_program(Command *cmd)
 {
-	/** TODO: Setup redirections.
-		*
-		* For each non-NULL `in_filename`, `out_filename` and `err_filename` field
-		* of `cmd`, perform the following steps:
-		*
-		*  - Use `open` to acquire a new file descriptor (make sure you have
-		*      correct flags and permissions)
-		*  - Use `dup2` to duplicate the newly opened file descriptors to the
-		*      standard I/O file descriptors (use the symbolic constants
-		*      STDOUT_FILENO, STDIN_FILENO and STDERR_FILENO.)
-		*  - Use `close` to close the opened file, so as to not leave an open
-		*      descriptor across an exec*() call
-		*/
-
-	/* TODO: Use `execvp` to replace current process with the command program
-		*
-		* In the case of an error, use `perror` to indicate the name of the
-		* command that failed.
-		*/
+	
 	if(cmd->in_filename){
 		int fd = open(cmd->in_filename, O_RDONLY, 0644);
 		if (fd < 0) {
@@ -221,39 +187,6 @@ static int execute_command_with_pipe(Command *cmd)
 {
 	assert (cmd->pipe_to != NULL);
 
-	/* TODO: Use `pipe(2)` to create a pair of connected file descriptors.
-		*
-		* These will be used to attach the STDOUT of one command to the STDIN of
-		* the next.
-		*
-		* Be sure to check for and report any errors when creating the pipe.
-		*/
-
-	/* TODO: Fork a new process.
-
-		* In the child:
-		*      - Close the read end of the pipe
-		*      - Close the STDOUT descriptor
-		*      - Connect STDOUT to the write end of the pipe (the one you didn't close)
-		*      - Call `run_program` to run this command
-		*
-		*  In the parent:
-		*      - Fork a second child process
-		*      - In child 2:
-		*          - Close the write end of the pipe
-		*          - Close the STDIN descriptor
-		*          - Connect STDIN to the read end of the pipe
-		*          - Call `execute_command` to execute the `pipe_to` command
-		*      - In the parent:
-		*          - Close both ends of the pipe
-		*          - Wait for both children to terminate.
-		*
-		* NOTE: This is a recursive approach. You may find it illuminating instead
-		* to consider how you could implement the execution of the whole pipeline
-		* in an interative style, using only a single parent, with one child per
-		* command. If you wish, feel free to implement this instead of what is
-		* outlined above.
-		*/
 	int pipefds[2];
 	if (pipe(pipefds) < 0) {
 		perror("pipe");
